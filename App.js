5c89d4c0cfe0fc74ef6863c2e7e050d099914806
rayancavalcante12345-tech <rayancavalcante12345@gmail.com>
2026-09-15 16:29:26 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="perfil">
+      <h2 className="nome">Maria</h2>
+      <p className="descricao">Desenvolvedora Front-end</p>
+      <button className="botao">Ver perfil</button>
+    </div>
+  );
+}
